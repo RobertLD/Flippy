c268548ea3421167e8b6abba3a0f7f8efe6d3ba3
David Hranicky <djh30000@gmail.com>
2021-09-04 21:43:24 -0400

    Changed the bot to be closer to the documentation Added function to read for messages

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import discord
-from discord.ext import commands
 from osrs_wiki_api import * #OSRS python wrapper
 
-client = commands.Bot(command_prefix = '.')
+client = discord.Client()
 TOKEN = 'ODgwOTExMjQxMzQ5MDM4MTQw.YSlKhg.4gkIWIqoh-3YHx0s67Dohu_7Wz4'
-@client.event
-async def on_ready():
-    print("Bot is ready.")
 
 @client.event
-async def on_ready():
-    print("Bot is ready.")
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello World!')
+    
+    if message.content.startswith('$kayah'):
+        await message.channel.send('Kayah is the best girl ever!')
+    
+    if message.content.startswith('$test'):
+        data = latest_prices()
+        await message.channel.send(data['data']['2']['high'])
+
 
 client.run(TOKEN)
