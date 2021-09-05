4ed24a95f2ba4d8ac08ceb428dce82e96f5e47fd
David Hranicky <djh30000@gmail.com>
2021-09-05 19:17:05 -0400

    Sanitize json so all keys are lowercase

--- get_item_map_by_name.py
+++ get_item_map_by_name.py
@@ -4,7 +4,7 @@ from osrs_wiki_api import *
 list = item_map()
 data = {}
 for val in list:
-    data[str(val['name'])] = val
+    data[str(val['name']).lower()] = val
 
 with open('item_map_name.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
