166a5f100349ea44d401a421156dfa07f20cecea
David Hranicky <djh30000@gmail.com>
2021-09-05 18:08:08 -0400

    Removed print statements

--- get_item_map.py
+++ get_item_map.py
@@ -6,8 +6,5 @@ data = {}
 for val in list:
     data[str(val['id'])] = val
 
-
-print(type(data))
-
 with open('item_map.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
